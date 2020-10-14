e166805c89a52877a54018ef5a39643f3c3963f1
Sneezycheesy <j-sjengske@hotmail.com>
2020-10-14 16:19:48 +0200

    Changed username to the right one

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
         private $db;
 
         public function __construct() {
-            $this->db = mysqli_connect('localhost', 'joshii', 'thispasswordisuseless', 'Verrukkulluk_db') or die(mysqli_connect_error());
+            $this->db = mysqli_connect('localhost', 'Joshii', 'thispasswordisuseless', 'Verrukkulluk_db') or die(mysqli_connect_error());
         }
 
         public function GetDatabaseConnection() {
