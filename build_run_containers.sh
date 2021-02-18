8245c2e8676e0ba708dbd4b490c591a2c0a36274
Sneezycheesy <j-sjengske@hotmail.com>
2021-02-18 09:02:49 +0100

    Reordered file structure

--- build_run_containers.sh
+++ build_run_containers.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-`git clone https://github.com/Sneezycheesy/Verrukkulluk_Joshua_Niks.git ./`;
-docker build -t verrukkulluk_app:latest;
-cd ./docker/Mysql/;
+git clone https://github.com/Sneezycheesy/Verrukkulluk_Joshua_Niks.git ./verrukkulluk;
+cd ./verrukkulluk
+cd ./php
+docker build -t verrukkulluk_app:latest .;
+cd ../
+cd ./mysql/;
 docker build -t verrukkulluk_mysql:latest .;
+cd ../
 
 
-docker run -dp 9906:3306 --name verrukkulluk_mysql verrukkulluk_mysql:latest;
+docker run -dp 9906:3306 --name verrukkulluk_mysql_test verrukkulluk_mysql:latest;
+sleep 30;
 
-sleep 5;
+docker exec -i verrukkulluk_mysql_test sh -c 'exec mysql -u root -p"thispasswordisuseless"' < ./php/verrukkulluk/database/Verrukkulluk_db.sql;
+docker exec -i verrukkulluk_mysql_test sh -c 'exec mysql -u root -p"thispasswordisuseless" -e "CREATE USER Joshii;"';
+docker exec -i verrukkulluk_mysql_test sh -c 'exec mysql -u root -p"thispasswordisuseless" -e "alter user Joshii identified by \"thispasswordisuseless\";"';
+docker exec -i verrukkulluk_mysql_test sh -c 'exec mysql -u root -p"thispasswordisuseless" -e "grant all privileges on Verrukkulluk.* to Joshii@\"%\";"';
 
-docker exec -i verrukkulluk_mysql_test sh -c 'exec mysql -u root -p"thispasswordisuseless"' < ${PWD}/Verrukkulluk_db.sql;
-docker exec -i verrukkulluk_mysql_test sh -c 'exec mysql -u root -p"thispasswordisuseless" -e "CREATE USER Joshii" -e "Alter user Joshii identified by thispasswordisuseless" -e "GRANT ALL PRIVILEGES Joshii@% identified by thispasswordisuseless"';
 
-
-docker run -d -p 8080:80 --name verrukkulluk_app verrukkulluk_app:latest;
+docker run -d -p 8000:80 --name verrukkulluk_app_test verrukkulluk_app:latest;
